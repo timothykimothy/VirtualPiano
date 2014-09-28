f1cab61dfc7b3822b71889034271953f862bdce8
andycyl <andydancouga1219@gmail.com>
2014-09-28 01:58:59 -0400

    Update Play.java

--- src/Play.java
+++ src/Play.java
@@ -1,25 +1,10 @@
-import java.util.HashMap;
 import org.jfugue.Player;
 
 public class Play {
-	private static HashMap<Integer, Character> pitch = new HashMap<Integer, Character>();
+	//private static HashMap<Integer, Character> pitchMap = new HashMap<Integer, Character>();
+	private static Player player= new Player();
 
-	public Play() {
-		pitch.put(1, 'C');
-		pitch.put(2, 'D');
-		pitch.put(3, 'E');
-		pitch.put(4, 'F');
-		pitch.put(5, 'G');
-		pitch.put(6, 'A');
-		pitch.put(7, 'B');
-		pitch.put(8, 'C');
-	}
-
-	public static void play(String note) {
-		Player player = new Player();
-		int h = Character.getNumericValue(note.charAt(0)) + 3;
-		char v = pitch.get(Character.getNumericValue(note.charAt(1)));
-		System.out.println("Note :" + v + h);
-		player.play(v + "" + h);
+	public void play(String pattern) {
+		player.play(pattern);
 	}
 }
